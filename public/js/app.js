fde95dc71929f73900b66cd9397f0ea6531843e7
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-10-05 19:48:36 -0400

    Created initial grid

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,19 @@
 (function() {
-  document.title = "Hiya!";
+
+  var numberOfRows = 40; //number of rows in the grid
+  var numberOfCols = 40; //number of columns in the grid
+  var canvas = $('#canvas'); //placement area - like paper for drawing
+
+  //let's make some rows and put them in the body
+   //'for' loop
+  for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1) {
+    var row = $('<tr></tr>');
+    for(var colCount = 0; colCount < numberOfCols; colCount +=1) {
+      var column = $('<td></td>');
+      column.addClass('cell');
+      row.append(column);
+    }
+    canvas.append(row);
+  }
 
 }());
