c10982227b7ada3f1b620f82b32672d14148d3ba
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-10-05 20:34:17 -0400

    Base functionality for changing cell color

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,29 @@
 (function() {
 
-  var numberOfRows = 40; //number of rows in the grid
-  var numberOfCols = 40; //number of columns in the grid
+  var numberOfRows = 15; //number of rows in the grid
+  var numberOfCols = 15; //number of columns in the grid
   var canvas = $('#canvas'); //placement area - like paper for drawing
 
-  //let's make some rows and put them in the body
-   //'for' loop
-  for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1) {
-    var row = $('<tr></tr>');
-    for(var colCount = 0; colCount < numberOfCols; colCount +=1) {
-      var column = $('<td></td>');
-      column.addClass('cell');
-      row.append(column);
+  makeGrid();
+  $('.cell').on('mouseover', changeColor);
+
+  function changeColor(event){
+    //'this' within an event refers to what triggered the event
+    $(this).toggleClass('red');
+  }
+
+  function makeGrid(){
+    //let's make some rows and put them in the body
+     //'for' loop
+    for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1) {
+      var row = $('<tr></tr>');
+      for(var colCount = 0; colCount < numberOfCols; colCount +=1) {
+        var column = $('<td></td>');
+        column.addClass('cell');
+        row.append(column);
+      }
+      canvas.append(row);
     }
-    canvas.append(row);
   }
 
 }());
