2b4fa06419414b07cc0f864a645335b96c2b4948
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-10-11 20:12:51 -0400

    Started formatting color picker and made functional

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,12 @@
   var updateGridButton = $('#update-grid-button');
   var numberOfRowsInput = $('#number-of-rows');
   var numberOfColsInput = $('#number-of-cols');
+  var newColor = "white";
+  var colorCell = $("#colors td");
+
 
   makeGrid(15, 15);
+  colorCell.on('click', selectColor);
   $('.cell').on('click', changeColor);
   updateGridButton.on('click', updateGridSize);
 
@@ -19,14 +23,32 @@
     var newColNumber = parseInt(numberOfColsInput.val());
     //grab number of rows input
     var newRowNumber = parseInt(numberOfRowsInput.val());
+
+    if(isNaN(newColNumber) || newColNumber < 1 || newColNumber > 50 || isNaN(newRowNumber) || newRowNumber < 1 || newRowNumber > 50){
+        newColNumber = 15;
+        newRowNumber = 15;
+        $('#error-message').text('Please enter a number between 1 and 50');
+    } else {
+      $('#error-message').text(' ');
+    }
     //make grid based off new numbers
     makeGrid(newRowNumber, newColNumber);
     $('.cell').on('click', changeColor);
   }
 
+  function selectColor(event){
+    newColor = $(this).attr("class");
+  }
+
   function changeColor(event){
     //'this' within an event refers to what triggered the event
-    $(this).toggleClass('red');
+    //$(this).toggleClass('red');
+
+    //changing color based off a color picker
+    $(this).removeClass();
+    $(this).addClass(function(){
+      return "cell " + newColor;
+    });
   }
 
   function makeGrid(numberOfRows, numberOfCols){
