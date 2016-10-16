b7e33c1323c131d063f1ae3e5a284782d9270f03
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-10-16 14:03:40 -0400

    Finished creation of hexagon grid and implemented

--- public/js/app.js
+++ public/js/app.js
@@ -1,79 +1,84 @@
 (function() {
-
   var canvas = $('#canvas'); //placement area - like paper for drawing
   var updateGridButton = $('#update-grid-button');
   var resetGridButton = $("#reset-grid-button");
   var numberOfRowsInput = $('#number-of-rows');
   var numberOfColsInput = $('#number-of-cols');
   var newColor = "white";
-  var colorCell = $("#colors td");
-  var newColNumber = 15;
+  var colorCell = $("#colors div");
+  var newColNumber = 30;
   var newRowNumber = 15;
 
   makeGrid(newRowNumber, newColNumber);
   colorCell.on('click', selectColor);
-  $('.cell').on('click', changeColor);
+  $('.hexagon').on('click', changeColor);
   updateGridButton.on('click', updateGridSize);
   resetGridButton.on('click', resetGrid);
 
-
   function clearGrid(){
     canvas.empty();
-  }
-
-  function updateGridSize(){
-    clearGrid();
-    //grab the number of columns input
-    newColNumber = parseInt(numberOfColsInput.val());
-    //grab number of rows input
-    newRowNumber = parseInt(numberOfRowsInput.val());
-
-    if(isNaN(newColNumber) || newColNumber < 1 || newColNumber > 50 || isNaN(newRowNumber) || newRowNumber < 1 || newRowNumber > 50){
-        newColNumber = 15;
-        newRowNumber = 15;
-        $('#error-message').text('Please enter a number between 1 and 50');
-    } else {
-      $('#error-message').text(' ');
-    }
-    //make grid based off new numbers
-    makeGrid(newRowNumber, newColNumber);
-    $('.cell').on('click', changeColor);
-    numberOfRowsInput.val("");
-    numberOfColsInput.val("");
-  }
-
-  function selectColor(event){
-    //'this' within an event refers to what triggered the event
-    newColor = $(this).attr("class");
-  }
-
-  function changeColor(event){
-
-    //changing color based off a color picker
-    $(this).removeClass();
-    $(this).addClass(function(){
-      return "cell " + newColor;
-    });
   }
 
   function makeGrid(numberOfRows, numberOfCols){
     //let's make some rows and put them in the body
      //'for' loop
     for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1) {
-      var row = $('<tr></tr>');
+      var row = $('<div class="hex-row"></div>');
       for(var colCount = 0; colCount < numberOfCols; colCount +=1) {
-        var column = $('<td></td>');
-        column.addClass('cell');
-        row.append(column);
+        var columnOdd = $('<div class="hexagon"><div class="left"></div><div class="middle"></div><div class="right"></div></div>');
+        var columnEven = $('<div class="hexagon even"><div class="left"></div><div class="middle"></div><div class="right"></div></div>');
+        row.append(function(){
+          if(colCount % 2 === 0){
+            return columnOdd;
+          } else {
+            return columnEven;
+          }
+        });
       }
       canvas.append(row);
     }
   }
 
+  function selectColor(event){
+    //'this' within an event refers to what triggered the event
+    newColor = $(this).css('border-left-color');
+  }
+
+  function changeColor(event){
+    $(this).children(".left").css('border-right-color', newColor);
+    $(this).children('.middle').css('border-color', newColor);
+    $(this).children('.right').css('border-left-color', newColor);
+  }
+
   function resetGrid(){
+    newColor = "white"
+
     clearGrid();
     makeGrid(newRowNumber, newColNumber);
-    $('.cell').on('click', changeColor);
+    $('.hexagon').on('click', changeColor);
+  }
+
+  function updateGridSize(){
+    newColor = "white"
+
+    clearGrid();
+    //grab the number of columns input
+    newColNumber = parseInt(numberOfColsInput.val());
+    //grab number of rows input
+    newRowNumber = parseInt(numberOfRowsInput.val());
+
+    if(isNaN(newColNumber) || newColNumber < 1 || newColNumber > 100 || isNaN(newRowNumber) || newRowNumber < 1 || newRowNumber > 100){
+        newColNumber = 30;
+        newRowNumber = 15;
+        $('#error-message').text('Please enter a number between 1 and 100');
+    } else {
+      $('#error-message').text(' ');
+    }
+    //make grid based off new numbers
+    makeGrid(newRowNumber, newColNumber);
+    $('.hexagon').on('click', changeColor);
+    numberOfRowsInput.val("");
+    numberOfColsInput.val("");
   }
 
 }());
